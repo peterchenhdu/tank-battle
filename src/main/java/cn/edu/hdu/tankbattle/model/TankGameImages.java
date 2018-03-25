3b4b29eeb7bdddb7755882331893ed2121d26a9e
cp_hdu@163.com <cp_hdu@163.com>
2018-03-25 19:25:26 +0800

    update...

--- src/main/java/cn/edu/hdu/tankbattle/model/TankGameImages.java
+++ src/main/java/cn/edu/hdu/tankbattle/model/TankGameImages.java
@@ -5,7 +5,6 @@
 package cn.edu.hdu.tankbattle.model;
 
 import java.awt.Image;
-import java.awt.Panel;
 import java.awt.Toolkit;
 
 /**
@@ -20,76 +19,76 @@ public class TankGameImages {
      */
     public static Image myTankImg[] = {
             Toolkit.getDefaultToolkit().getImage(
-                    Panel.class.getResource("/images/UTank_.gif")),
+                    TankGameImages.class.getResource("/static/UTank_.gif")),
             Toolkit.getDefaultToolkit().getImage(
-                    Panel.class.getResource("/images/DTank_.gif")),
+                    TankGameImages.class.getResource("/static/DTank_.gif")),
             Toolkit.getDefaultToolkit().getImage(
-                    Panel.class.getResource("/images/LTank_.gif")),
+                    TankGameImages.class.getResource("/static/LTank_.gif")),
             Toolkit.getDefaultToolkit().getImage(
-                    Panel.class.getResource("/images/RTank_.gif"))};
+                    TankGameImages.class.getResource("/static/RTank_.gif"))};
     /**
      * 敌人坦克四个方向图像数组
      */
     public static Image enemyTankImg[] = {
             Toolkit.getDefaultToolkit().getImage(
-                    Panel.class.getResource("/images/UTank.gif")),
+                    TankGameImages.class.getResource("/static/UTank.gif")),
             Toolkit.getDefaultToolkit().getImage(
-                    Panel.class.getResource("/images/DTank.gif")),
+                    TankGameImages.class.getResource("/static/DTank.gif")),
             Toolkit.getDefaultToolkit().getImage(
-                    Panel.class.getResource("/images/LTank.gif")),
+                    TankGameImages.class.getResource("/static/LTank.gif")),
             Toolkit.getDefaultToolkit().getImage(
-                    Panel.class.getResource("/images/RTank.gif"))};
+                    TankGameImages.class.getResource("/static/RTank.gif"))};
     /**
      * 障碍物图像数组
      */
     public static Image stuffImg[] = {
             Toolkit.getDefaultToolkit().getImage(
-                    Panel.class.getResource("/images/brick.gif")),
+                    TankGameImages.class.getResource("/static/brick.gif")),
             Toolkit.getDefaultToolkit().getImage(
-                    Panel.class.getResource("/images/iron.gif")),
+                    TankGameImages.class.getResource("/static/iron.gif")),
             Toolkit.getDefaultToolkit().getImage(
-                    Panel.class.getResource("/images/water.gif"))};
+                    TankGameImages.class.getResource("/static/water.gif"))};
     /**
      * 游戏开始图像
      */
     public static Image startImage = Toolkit.getDefaultToolkit().getImage(
-            Panel.class.getResource("/images/gameStart.png"));
+            TankGameImages.class.getResource("/static/gameStart.png"));
     /**
      * 子弹图像
      */
     public static Image bullet = Toolkit.getDefaultToolkit().getImage(
-            Panel.class.getResource("/images/bullet.gif"));
+            TankGameImages.class.getResource("/static/bullet.gif"));
     /**
      * 爆炸图像
      */
     public static Image bomb[] = {
             Toolkit.getDefaultToolkit().getImage(
-                    Panel.class.getResource("/images/bomb_1.png")),
+                    TankGameImages.class.getResource("/static/bomb_1.png")),
             Toolkit.getDefaultToolkit().getImage(
-                    Panel.class.getResource("/images/bomb_2.png")),
+                    TankGameImages.class.getResource("/static/bomb_2.png")),
             Toolkit.getDefaultToolkit().getImage(
-                    Panel.class.getResource("/images/bomb_3.png")),
+                    TankGameImages.class.getResource("/static/bomb_3.png")),
             Toolkit.getDefaultToolkit().getImage(
-                    Panel.class.getResource("/images/bomb_4.png")),
+                    TankGameImages.class.getResource("/static/bomb_4.png")),
             Toolkit.getDefaultToolkit().getImage(
-                    Panel.class.getResource("/images/bomb_5.png")),};
+                    TankGameImages.class.getResource("/static/bomb_5.png")),};
     /**
      * 游戏失败时图像
      */
     public static Image gameOver = Toolkit.getDefaultToolkit().getImage(
-            Panel.class.getResource("/images/gameOver.gif"));
+            TankGameImages.class.getResource("/static/gameOver.gif"));
     /**
      * 胜利时的图像
      */
     public static Image gameWin = Toolkit.getDefaultToolkit().getImage(
-            Panel.class.getResource("/images/gameWin.gif"));
+            TankGameImages.class.getResource("/static/gameWin.gif"));
     /**
      * 洋葱头 笑
      */
     public static Image yct_smile1 = Toolkit.getDefaultToolkit().getImage(
-            Panel.class.getResource("/images/yct1.png"));
+            TankGameImages.class.getResource("/static/yct1.png"));
     public static Image yct_smile2 = Toolkit.getDefaultToolkit().getImage(
-            Panel.class.getResource("/images/yct2.png"));
+            TankGameImages.class.getResource("/static/yct2.png"));
     public static Image font = Toolkit.getDefaultToolkit().getImage(
-            Panel.class.getResource("/images/font.png"));
+            TankGameImages.class.getResource("/static/font.png"));
 }
